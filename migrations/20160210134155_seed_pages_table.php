568330dd767a13d1c72cb807125ca451712e4b56
eCarrot <ervinselimovic@Ervins-MacBook-Pro.local>
2016-03-01 14:00:55 +0100

    Delete and comment out some things that are not needed

--- migrations/20160210134155_seed_pages_table.php
+++ migrations/20160210134155_seed_pages_table.php
@@ -1,23 +1,23 @@
 <?php
 
-use Phinx\Migration\AbstractMigration;
+// use Phinx\Migration\AbstractMigration;
 
-class SeedPagesTable extends AbstractMigration
-{
-    public function up()
-    {
-        $this->execute("
-            insert into pages (user_id, browser_title, title, page_content, picture)
-            values (1, 
-                    'TestPage', 
-                    'TestPageTitle',
-                    'TestPageContent',
-                    'test.jpg')
-        ");
-    }
+// class SeedPagesTable extends AbstractMigration
+// {
+//     public function up()
+//     {
+//         $this->execute("
+//             insert into pages (user_id, browser_title, title, page_content, picture)
+//             values (1, 
+//                     'TestPage', 
+//                     'TestPageTitle',
+//                     'TestPageContent',
+//                     'test.jpg')
+//         ");
+//     }
 
-    public function down()
-    {
+//     public function down()
+//     {
 
-    }
-}
+//     }
+// }
