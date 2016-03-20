3256b4204b1d8f3e69d607a3ea53b11c884bfe7b
Ervin Selimovic <ervinselimovic10@gmail.com>
2016-03-21 00:09:26 +0100

    Creditor config 1

--- public/admin/ckeditor/config.js
+++ public/admin/ckeditor/config.js
@@ -7,6 +7,8 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here.
   config.extraPlugins = 'insertpre';
   CKEDITOR.config.insertpre_class = 'terminal';
+
+  config.fillEmptyBlocks = false;
   //CKEDITOR.config.insertpre_class = 'code';
 	// For complete reference see:
 	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
